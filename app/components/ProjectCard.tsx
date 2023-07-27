196b57787e51899e89b09c688eb18e53d376acc1
Mario Sanseverino <marioaugusto.sanseverino@gmail.com>
2023-07-27 15:03:00 -0300

    Added a hover transition from grayscale to colorized on the ProjectCard component

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -11,7 +11,7 @@ export default function ProjectCard(project: IProject) {
 	return (
 		<div className={ `w-96 mb-6 mx-auto justify-center pb-4 text-center bg-black text-white rounded-lg ${ theme === 'dark' ? 'drop-shadow-md' : '' }` }>
 			<a href={ project.link} target='_blank' rel="noreferrer">
-				<img src={ project.thumbnail} alt="Project Thumbnail" className='w-96 rounded-t-lg' />
+				<img src={ project.thumbnail} alt="Project Thumbnail" className='w-96 rounded-t-lg grayscale transition duration-500 ease-in-out transform hover:grayscale-0' />
 				<h1 className='my-1 font-semibold text-lg'>{ project.title }</h1>
 				{ project.stack.map((stack, index) => (
 					<span

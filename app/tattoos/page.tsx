0171054ea345d74bde1b0e7ec26f7060c495a2bb
Mario Sanseverino <marioaugusto.sanseverino@gmail.com>
2024-02-20 14:57:59 -0300

    fix: /tattoos on lg with decent gap

--- app/tattoos/page.tsx
+++ app/tattoos/page.tsx
@@ -5,7 +5,7 @@ import { tattooList } from '@/public/images/tattoos/tattooList';
 
 export default function Tattoos() {
 	return (
-		<section className='px-2 py-4 mx-auto w-10/12 grid grid-cols-2 bg-white'>
+		<section className='w-10/12 px-2 py-4 mx-auto grid grid-cols-2 bg-white gap-2 lg:grid-cols-3 lg:px-4 lg:gap-4'>
 			{ tattooList.map(({ name, date }, index) => (
 				<TattooCard
 					key={ index }

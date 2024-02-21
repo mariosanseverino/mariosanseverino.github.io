18dc9385b73509b1ff3f3bae5096faf4eafa93c8
Mario Sanseverino <marioaugusto.sanseverino@gmail.com>
2024-02-21 16:13:32 -0300

    fix: Misspeling

--- app/tattoos/page.tsx
+++ app/tattoos/page.tsx
@@ -6,7 +6,7 @@ import TattooLibrary from '../components/TattooLibrary';
 export default function Tattoos() {
 	return (
 		<section className='h-full w-full p flex flex-col justify-center items-center gap-8'>
-			<h1 className={ `uppercase text-6xl md:text-9xl ${ mohave.className }` }><span className={ unifraktur.className }>T</span>attos</h1>
+			<h1 className={ `uppercase text-6xl md:text-9xl ${ mohave.className }` }><span className={ unifraktur.className }>T</span>attoos</h1>
 			<TattooLibrary />
 		</section>
 	);
